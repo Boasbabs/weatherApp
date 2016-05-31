5f46518cf28d84954ff662bae4da73aecb882c11
Boasbabs <babalolasimeon@gmail.com>
2016-05-31 15:16:57 +0530

    set up routes path

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 //MODULE
-var weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
+var weatherApp = angular.module("weatherApp",["ngRoute", "ngResource"]);
+
+weatherApp.config(function($routeProvider) {
+	$routeProvider
+
+	.when("/", {
+		templateUrl: "pages/main.html",
+		controller: "mainController"
+	})
+	.when("/forecast", {
+		templateUrl: "pages/forecast.html",
+		controller: "forecastController"
+	})
+});
+
+weatherApp.controller("mainController", ["$scope", function ($scope) {
+	//$scope.name = "John Doe";
+}]);
+weatherApp.controller("forecastController", ["$scope", function ($scope) {
+	//$scope.name = "Forecast Details";
+}]);
