d0a5c50c1a6b680bb9ece53ca7b42d75d0af77ce
Boasbabs <babalolasimeon@gmail.com>
2016-06-01 01:52:12 +0530

    add weather API

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ weatherApp.config(function($routeProvider) {
 // SERVICES
 weatherApp.service("cityService", function() {
 
-	this.city = "New York, NY";
+	this.city = "London, UK";
 });
 
 // CONTROLLERS
@@ -29,6 +29,13 @@ weatherApp.controller("mainController", ["$scope", "cityService", function ($sco
 		cityService.city = $scope.city;
 	});
 }]);
-weatherApp.controller("forecastController", ["$scope", "cityService", function($scope, cityService) {
+weatherApp.controller("forecastController", ["$scope", "$resource", "cityService", function($scope, $resource, cityService) {
 	$scope.city = cityService.city;
+
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/city?q=London&APPID=27d43832d2a4adcb97fcbfa23db130aa", {
+		callback: "JSON_CALLBACK"}, { get: { method: "JSONP"}});
+
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 5});
+
+	console.log($scope.weatherResult);
 }]);
