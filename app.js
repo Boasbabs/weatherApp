7687671b935b21732e0307f88372b9d0a545571b
Boasbabs <babalolasimeon@gmail.com>
2016-06-01 13:34:32 +0530

    Fix api to get daily forecast of specific city not 3 hours interval and change temp to C

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //MODULE
 var weatherApp = angular.module("weatherApp",["ngRoute", "ngResource"]);
 
-// ROUTES
+//ROUTES
 weatherApp.config(function($routeProvider) {
 	$routeProvider
 
@@ -13,11 +13,15 @@ weatherApp.config(function($routeProvider) {
 		templateUrl: "pages/forecast.html",
 		controller: "forecastController"
 	})
+	.when("/forecast/:days", {
+		templateUrl: "pages/forecast.html",
+		controller: "forecastController"
+	})
 });
 // SERVICES
 weatherApp.service("cityService", function() {
 
-	this.city = "London, UK";
+	this.city = "";
 });
 
 // CONTROLLERS
@@ -29,13 +33,25 @@ weatherApp.controller("mainController", ["$scope", "cityService", function ($sco
 		cityService.city = $scope.city;
 	});
 }]);
-weatherApp.controller("forecastController", ["$scope", "$resource", "cityService", function($scope, $resource, cityService) {
+weatherApp.controller("forecastController", ["$scope", "$resource", "$routeParams", "cityService", function($scope, $resource, $routeParams, cityService) {
 	$scope.city = cityService.city;
 
-	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/city?q=London&APPID=27d43832d2a4adcb97fcbfa23db130aa", {
+	$scope.days = $routeParams.days || 2;
+
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?&APPID=27d43832d2a4adcb97fcbfa23db130aa", {
 		callback: "JSON_CALLBACK"}, { get: { method: "JSONP"}});
 
-	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 5});
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days});
+
+	$scope.convertToCelsius = function(degK) {
+		return Math.round(degK - 273.15);
+		// this is for FAHRENHEIT return Math.round((1.8 * (degK - 273)) + 32);
+	};
+	$scope.convertToDate = function(dt) {
+		return new Date(dt * 1000);
+	}
 
 	console.log($scope.weatherResult);
 }]);
+
+	//$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?q='{{$scope.city}}'&APPID=27d43832d2a4adcb97fcbfa23db130aa",
