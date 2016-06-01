d2f411cec611dcc4666c797c3aa08e4b1fadca94
Boasbabs <babalolasimeon@gmail.com>
2016-06-01 16:11:37 +0530

    Split application into different files

---
+++ controllers.js
@@ -0,0 +1,29 @@
+// CONTROLLERS
+weatherApp.controller("mainController", ["$scope", "cityService", function ($scope, cityService) {
+
+	$scope.city = cityService.city;
+
+	$scope.$watch("city", function () {
+		cityService.city = $scope.city;
+	});
+}]);
+weatherApp.controller("forecastController", ["$scope", "$resource", "$routeParams", "cityService", function($scope, $resource, $routeParams, cityService) {
+	$scope.city = cityService.city;
+
+	$scope.days = $routeParams.days || "2";
+
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?&APPID=27d43832d2a4adcb97fcbfa23db130aa", {
+		callback: "JSON_CALLBACK"}, { get: { method: "JSONP"}});
+
+	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days});
+
+	$scope.convertToCelsius = function(degK) {
+		return Math.round(degK - 273.15);
+		// this is for FAHRENHEIT return Math.round((1.8 * (degK - 273)) + 32);
+	};
+	$scope.convertToDate = function(dt) {
+		return new Date(dt * 1000);
+	}
+
+	console.log($scope.weatherResult);
+}]);
